3294440ee5fd7ed58c46bc420cace558a0de7c94
Jimmy Salazar <jimmyr.salazar@gmail.com>
2022-01-02 14:21:22 -0500

    Updated to Tailwind 3.0.8

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  prefix: "",
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
   },
